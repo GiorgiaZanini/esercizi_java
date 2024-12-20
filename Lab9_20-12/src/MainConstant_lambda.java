2d784668ba5529dd067a6d626873ad203f861833
GiorgiaZanini <giorgia.zanini.2104@gmail.com>
2024-12-20 17:16:54 +0100

    5.1.
    constant con lambda

--- Lab9_20-12/src/MainConstant_lambda.java
+++ Lab9_20-12/src/MainConstant_lambda.java
@@ -1,14 +1,17 @@
 public class MainConstant_lambda {
-    private static Board board = new Board(40, 20, 100);
-
     public static void main(String[] args) throws InterruptedException {
-        board.play(new ProcessorEquals());
-    }
+        Board board = new Board(40, 20, 100);
 
-    private static class ProcessorEquals implements NextAliveProcessor {
-        @Override
-        public boolean isAliveNextAt(int x, int y) {
-            return board.isAliveAt(x,y);
-        }
+        /*
+        board.play(new NextAliveProcessor() {   // classe anonima
+            @Override
+            public boolean isAliveNextAt(int x, int y) {
+                return board.isAliveAt(x,y);
+            }
+        });
+        */
+
+        board.play((x, y) -> board.isAliveAt(x,y));
+        //board.play(board::isAliveAt);
     }
 }
