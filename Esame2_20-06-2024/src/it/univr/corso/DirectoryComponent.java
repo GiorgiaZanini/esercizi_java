58715221c781a582f7ce460eea8d0ca554532e27
GiorgiaZanini <giorgia.zanini.2104@gmail.com>
2025-01-14 10:34:23 +0100

    Esame2
    consegna

---
+++ Esame2_20-06-2024/src/it/univr/corso/DirectoryComponent.java
@@ -0,0 +1,15 @@
+package it.univr.corso;
+
+public class DirectoryComponent extends AbstractComponent {
+	// aggiungete campi, se servissero
+
+	/**
+	 * Costruisce una componente di tipo directory con il nome indicato
+	 * e le sottocomponenti (figli) indicate.
+	 */
+	public DirectoryComponent(String name, Component... children) {
+		// completate
+	}
+
+	// implementate sotto i metodi public ancora astratti
+}
---
+++ Esame2_20-06-2024/src/it/univr/corso/FileComponent.java
@@ -0,0 +1,15 @@
+package it.univr.corso;
+
+public class FileComponent extends AbstractComponent {
+	// aggiungete campi, se servissero
+
+	/**
+	 * Costruisce una componente di tipo file con il nome indicato
+	 * e la dimensione in byte indicata.
+	 */
+	public FileComponent(String name, int size) {
+		// completate
+	}
+
+	// implementate sotto i metodi public ancora astratti
+}
